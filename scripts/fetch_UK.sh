e71ac24c61028b1293c6d69bf0cae81a2b5c2201
harrisonzhu508 <harrisonz@ma-hbz15.ma.ic.ac.uk>
2020-04-03 23:03:58 +0100

    add make all instructions and edit uk file]

--- scripts/fetch_UK.sh
+++ scripts/fetch_UK.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget --no-check-certificate -O ../data/UK/cases.xlsx 'https://fingertips.phe.org.uk/documents/Historic%20COVID-19%20Dashboard%20Data.xlsx'
+wget --no-check-certificate -O ../data/UK/uk.xlsx 'https://fingertips.phe.org.uk/documents/Historic%20COVID-19%20Dashboard%20Data.xlsx'
