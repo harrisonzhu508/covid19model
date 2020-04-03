878ef1de7b6faa907fdd0780a446b683f40d947c
harrisonzhu508 <harrisonz@ma-hbz15.ma.ic.ac.uk>
2020-04-03 23:32:42 +0100

    add usa pipeline

--- scripts/fetch_USA.sh
+++ scripts/fetch_USA.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-
+wget --no-check-certificate -O ../data/USA/usa.csv 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
