7404494cff486a34846f0579bdded3fb5802015b
harrisonzhu508 <harrisonz@ma-hbz15.ma.ic.ac.uk>
2020-04-03 22:53:17 +0100

    add in uk xlsx

--- R/make.ages_US.R
+++ R/make.ages_US.R
@@ -1,13 +1,11 @@
 library(tidyverse)
 library(data.table)
 
-indir = "~/Downloads" # path directory
-
 ## LOAD DATA FROM USA CENSUS 
 # download csv from URL bellow and save into path directory
 #https://data.census.gov/cedsci/table?q=age%20state&hidePreview=true&tid=ACSST1Y2018.S0101&t=Age%20and%20Sex&vintage=2018&cid=S0101_C01_001E&g=0100000US.04000.001&y=2018&moe=false&tp=false
 
-data = read.csv(file = file.path(indir, "export.csv"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
+data = read.csv(file = file.path("data/USA/export.csv"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
 ## GROUP BY 10 Y AGE BAND
 df = data.table(AGE = data[which(data[,3] == "AGE"),1][-1], data[which(data[,3] == "AGE"),which(as.character(data[1,]) == "Total")][-1,]) %>%
@@ -31,4 +29,4 @@ df = data.table(AGE = data[which(data[,3] == "AGE"),1][-1], data[which(data[,3] 
 ## CONVERT FROM LONG TO WIDE FORMAT
 df.wide = df %>%
   reshape2::dcast(STATE ~ AGE, value.var="POP")
-write.csv(df.wide,file.path(indir,"ages.csv"), row.names = FALSE)
+write.csv(df.wide,"data/USA/ages.csv", row.names = FALSE)
