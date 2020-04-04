64588efd8e137314892c5eaab71c35267e9d10c7
harrisonzhu508 <harrisonz@ma-hbz15.ma.ic.ac.uk>
2020-04-04 11:07:09 +0100

    add italy

---
+++ scripts/fetch_Belgium.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget --no-check-certificate -O ../data/Belgium/belgium.csv 'https://epistat.sciensano.be/Data/COVID19BE_MORT.csv'
--- scripts/fetch_USA.sh
+++ scripts/fetch_USA.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget --no-check-certificate -O ../data/USA/usa.csv 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
+wget --no-check-certificate -O ../data/Italy/mobility.csv 'https://data.humdata.org/dataset/40a9ea9e-0edb-49f7-a440-6aee3015961b/resource/d5384152-f198-466d-9be8-7f250c4edae9/download/average_network_degree_by_province_20200321_allusers.csv'
