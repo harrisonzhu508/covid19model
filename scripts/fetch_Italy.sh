c260d00cfdb1075565f19f61dc6eb7591ae49ee7
harrisonzhu508 <harrisonz@ma-hbz15.ma.ic.ac.uk>
2020-04-04 11:14:42 +0100

    fix script

--- scripts/fetch_Italy.sh
+++ scripts/fetch_Italy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget --no-check-certificate -O ../data/Belgium/belgium.csv 'https://epistat.sciensano.be/Data/COVID19BE_MORT.csv'
+wget --no-check-certificate -O ../data/Italy/mobility.csv 'https://data.humdata.org/dataset/40a9ea9e-0edb-49f7-a440-6aee3015961b/resource/d5384152-f198-466d-9be8-7f250c4edae9/download/average_network_degree_by_province_20200321_allusers.csv'
